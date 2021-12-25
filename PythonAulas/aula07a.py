672aa79456d4cdfc91ffe56000e2bdc2542438b4
tiagovianez <tiagovianez@live.com>
2021-12-25 17:50:55 -0300

    Ordem de precedência

---
+++ PythonAulas/aula07a.py
@@ -0,0 +1,19 @@
+#Ordem de precedência
+#1. ()
+#2. **
+#3. * / // %
+#4. + -
+
+#PRÁTICA
+# n = input('Digite um nome: ')
+# print('Prazer em lhe conhecer, {}!'.format(n))
+
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite um outro valor: '))
+s = n1 + n2
+m = n1 * n2
+d = n1 / n2
+di = n1 // n2
+e = n1 ** n2
+print('A soma é {}, \nO produto é {} e a \nDivisão é {}'.format(s, m, d))
+print('A divisão inteira é {} e a potência {}'.format(di, e))
