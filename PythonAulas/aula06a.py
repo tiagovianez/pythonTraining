f62f3aba8a8322b1aaf73db9a757ce2100d69499
tiagovianez <tiagovianez@live.com>
2021-12-25 17:16:20 -0300

    Aulas e Teorias

---
+++ PythonAulas/aula06a.py
@@ -0,0 +1,13 @@
+#IntTypes: 7, -4, 0, 9875
+#floatTypes: 4.5, 0.076, -15.34498, 7.0
+#boolTypes: True, False
+#strTypes: 'Hello', '7.5'
+
+#Teste1 - Valores inteiros
+n1 = int(input('Digite o primeiro numero: '))
+n2 = int(input('Digite o segundo numero: '))
+s = n1 + n2
+#print('O resultado da soma é: {}'.format(soma))
+print('A soma entre {} e {} vale {}'.format(n1, n2, s))
+
+
---
+++ PythonAulas/aula06b.py
@@ -0,0 +1,16 @@
+#Utilizando valores flutuantes
+n = float(input('Digite um valor: '))
+print(n)
+
+#Utilizando valores booleanos: Se for digitado um valor, a resposta será 'True', caso contrário 'False'
+n = bool(input('Digite um valor: '))
+print(n)
+
+#Testando se um valor é númérico ou não
+n = str(input('Digite um valor: '))
+print(n.isnumeric())
+
+#Verificando de uma palavra dita está em letra maiúscula
+n = str(input('Digite algo: '))
+print(n.isupper())
+print((n.i))
