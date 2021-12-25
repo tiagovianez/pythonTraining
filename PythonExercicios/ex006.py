b67de745ea312ef523591546c6009d0f4f5241d3
tiagovianez <tiagovianez@live.com>
2021-12-25 17:42:13 -0300

    Dobro, Triplo e Raiz Quadrada

---
+++ PythonExercicios/ex006.py
@@ -0,0 +1,6 @@
+#Crie um algorítmo que leia um numero e mostre o seu dobro, triplo e raiz quadrada.
+n = int(input('Digite um número: '))
+d = n * 2
+t = n * 3
+rq = n ** 0.5
+print('O dobro desse valor é: {}\nSeu triplo é: {}\nSua raiz quadrada é: {}'.format(d, t, rq))
