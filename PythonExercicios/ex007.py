6f58713fd15c2b5122d801110d9fe9deea255aea
tiagovianez <tiagovianez@live.com>
2021-12-25 17:42:49 -0300

    Média de notas

---
+++ PythonExercicios/ex007.py
@@ -0,0 +1,6 @@
+#Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média
+#Ps.: Ordem de precedência
+n1 = int(input('Digite a primeira nota: '))
+n2 = int(input('Digite a segunda nota: '))
+m = (n1 + n2) / 2
+print('A média desse aluno é: {}'.format(m))
