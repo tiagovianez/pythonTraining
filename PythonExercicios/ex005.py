0d8469cd536fc8b56a4cd864e83eb351000de336
tiagovianez <tiagovianez@live.com>
2021-12-25 17:41:49 -0300

    Antecessor e sucessor

---
+++ PythonExercicios/ex005.py
@@ -0,0 +1,5 @@
+#Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor.
+n = int(input('Digite um valor: '))
+t1 = n - 1
+t2 = n + 1
+print('Seu antecessor é {}\nO seu sucessor é {}'.format(t1, t2))
