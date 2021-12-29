dd5a317e1023e1d4fb656b0da4b63d1a6a85aa44
tiagovianez <tiagovianez@live.com>
2021-12-28 23:08:52 -0300

    Desconto de produto

--- PythonExercicios/ex012.py
+++ PythonExercicios/ex012.py
@@ -1,4 +1,4 @@
 #Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
-p1 = float(input('Digite o preço: '))
+p1 = float(input('Digite o preço: R$'))
 p2 = (p1 * 0.95)
 print('O preço desse produto com desconto de 5% fica: R${:.2f}'.format(p2))
