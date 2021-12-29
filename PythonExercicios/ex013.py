0f3886092d20dc4c693dd694e6f7de74811fd24e
tiagovianez <tiagovianez@live.com>
2021-12-28 23:09:10 -0300

    Aumento de salário

--- PythonExercicios/ex013.py
+++ PythonExercicios/ex013.py
@@ -1,4 +1,4 @@
 #Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
-s1 = float(input('Digite o salário: '))
+s1 = float(input('Digite o salário atual: R$'))
 s2 = (s1 * 0.15) + s1
-print('O seu novo salário é: R${:.2f}'.format(s2))
+print('Um funcionário que ganhava R${:.2f}, passará a ganhar R${:.2f}'.format(s1, s2))
