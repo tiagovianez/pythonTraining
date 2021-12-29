05166e7c0faf03084773828cfa8b889b1643c680
tiagovianez <tiagovianez@live.com>
2021-12-28 23:08:29 -0300

    Densidade, área e litros

--- PythonExercicios/ex011.py
+++ PythonExercicios/ex011.py
@@ -1,8 +1,9 @@
 #Faça um programa que leia a largura e a altura de uma parede em metros, calcule a sua área e a quantidade de tinta
 #necessária para pintá-lo, sabendo que cada litro de tinta, pinta uma área de 2m².
-l = int(input('Digite a largura: '))
-a = int(input('Digite sua altura: '))
+l = float(input('Largura da parede: '))
+a = float(input('Altura da parede: '))
 area = l * a
-ql = 2 * area
-print('A área da parede é: {}m²\n'
-      'A quantidade de tinta utilizada foi: {} litros\n'.format(area, ql))
+ql = (area / 2)
+print('Sua parede tem a dimensão de {}x{}\n'
+      'Sua área é de {:.2f}m²\n'
+      'A quantidade de tinta a ser utilizada é {:.2f} litros\n'.format(l, a, area, ql))
