33eaddb5731b8fc6c8caf3c660500403deb32d6f
tiagovianez <tiagovianez@live.com>
2021-12-28 23:07:35 -0300

    Conversor de moeda

--- PythonExercicios/ex010.py
+++ PythonExercicios/ex010.py
@@ -1,5 +1,5 @@
 #Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos Doláres ela pode comprar.
 #Considere: US$1,00 = R$ 3,27
-n = int(input('Digite o valor em reais presente na carteira do cliente: '))
+n = float(input('Digite o valor em reais presente na carteira do cliente: R$'))
 d = (n / 3.27)
-print('A quantidade em dólar que o cliente pode adquirir é US$ {:.2f}'.format(d))
+print('Com R$ {:.2f} você pode comprar: US$ {:.2f}'.format(n, d))
