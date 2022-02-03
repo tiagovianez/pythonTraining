aa396bfdf0c5fad85bbaf507b69a6875c9e3b929
tiagovianez <tiagovianez@live.com>
2022-02-03 17:43:03 -0300

    Importando a biblioteca Math

---
+++ PythonAulas/aula08a.py
@@ -0,0 +1,5 @@
+#Importando a biblioteca Math e calculando a raiz quadrada de um número.
+from math import sqrt, floor, ceil
+n = int(input('Digite um numero: '))
+raiz = sqrt(n)
+print('A raiz de {} é igual a {:.2f}'.format(n, sqrt(n)))
