611abb4b18869388daf379abed6e2f03332f0e77
tiagovianez <tiagovianez@live.com>
2022-02-03 23:24:28 -0300

    Porção inteira de um número

--- PythonExercicios/ex016.py
+++ PythonExercicios/ex016.py
@@ -1,1 +1,7 @@
-#Crie um programa que leia um numero real qualquer pelo teclado e mostre na tela a sua porção inteira
+#Crie um programa que leia um numero real qualquer pelo teclado e mostre na tela a sua porção inteira;
+#Arredondamento para cima e para baixo
+from math import trunc, floor, ceil
+n = float(input('Digite um valor: '))
+print('O valor {} em inteiro corresponde a: {}'.format(n, trunc(n)))
+print('O valor {} arredondado para cima corresponde a: {}'.format(n, ceil(n)))
+print('O valor {} arredondado para baixo corresponde a: {}'.format(n, floor(n)))
