93cc805e11d360fb597f8a553d4b5e780d70ae22
tiagovianez <tiagovianez@live.com>
2022-02-03 23:24:56 -0300

    Comprimento da hipotenusa

--- PythonExercicios/ex017.py
+++ PythonExercicios/ex017.py
@@ -1,2 +1,6 @@
 #Faça um programa que leia o comprimento do cateto oposto e do cateto adjacenete de um triangulo retangulo, calcule e mostre
 #o comprimento da hipotenusa.
+import math
+co = float(input('Comprimento do cateto oposto: '))
+ca = float(input('Comprimento do cateto adjacente: '))
+print('O valor da hipotenusa é: {:.2f}'.format(math.hypot(co, ca)))
