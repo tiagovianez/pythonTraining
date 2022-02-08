5b38a1cc1171a3100c8b3ce0b3eb1d00c1e0d812
tiagovianez <tiagovianez@live.com>
2022-02-08 09:17:57 -0300

    Verificando as primeiras letras de um texto

--- PythonExercicios/ex024.py
+++ PythonExercicios/ex024.py
@@ -1,6 +1,5 @@
 #Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome "SANTO".
 #Removendo os espaços do ínicio e fim
 cidade = str(input('Digite o nome da cidade: ')).strip()
-print('Analisando o nome da cidade... ')
-##Ele vai verificar se tem Santo e jogar a palavra para caixa alta.
-print('Essa cidade se inicia com o nome Santo?: {}'.format(cidade[:5].upper() == 'SANTO'))
+print('Analisando o nome da cidade...')
+print('O nome da cidade tem SANTO? {}'.format(cidade[:5].upper() == 'SANTO'))
