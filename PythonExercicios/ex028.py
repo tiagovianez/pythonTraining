08fd48abc3a920f8605bf3aa466f239520e15087
tiagovianez <tiagovianez@live.com>
2022-02-10 16:28:22 -0300

    Adivinhe qual foi o valor pensado

--- PythonExercicios/ex028.py
+++ PythonExercicios/ex028.py
@@ -2,7 +2,7 @@
 # DESCOBRIR QUAL FOI O NÚMERO ESCOLHIDO PELO COMPUTADOR.
 # O PROGRAMA DEVERÁ ESCREVER NA TELA SE O USUÁRIO VENCEU OU PERDEU
 
-n = int(input('Adivinhe qual foi o número pensando pelo computador de 0 a 5: '))
+n = int(input('Adivinhe qual foi o número pensado pelo computador de 0 a 5: '))
 if n == 4:
     print('Parabéns, Você venceu!')
 elif n > 5:
