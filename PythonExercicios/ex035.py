ee701c01998b1d5c628874cd6091858488a2df8e
tiagovianez <tiagovianez@live.com>
2022-02-11 09:40:02 -0300

    Formando um triângulo

--- PythonExercicios/ex035.py
+++ PythonExercicios/ex035.py
@@ -1,8 +1,11 @@
 #DESENVOLVA UM PROGRAMA QUE LEIA O COMPRIMENTO DE TRÊS RETAS E DIGA AO USUÁRIO SE ELAS PODEM OU NÃO FORMAR UM TRIÂNGULO.
-a = float(input('Digite o primeiro comprimento da reta: '))
-b = float(input('Digite o segundo comprimento da reta: '))
-c = float(input('Digite o terceiro comprimento da reta: '))
-if a + b > c:
-    print('Podemos formar um triângulo!')
+print('-='*20)
+print('Analisador de Triângulos')
+print('-='*20)
+a = float(input('Digite o primeiro segmento: '))
+b = float(input('Digite o segundo segmento: '))
+c = float(input('Digite o terceiro segmento: '))
+if a + b > c and a + c > b and c + b > a:
+    print('PODEMOS FORMAR um triângulo, com os segmentos citados!')
 else:
-    print('Não é possível formar um triângulo')
+    print('NÃO É POSSÍVEL formar um triângulo!')
