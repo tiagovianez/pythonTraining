94e89ad26558bf8f61a4df3cd02c0d775a9c75b0
tiagovianez <tiagovianez@live.com>
2022-03-02 20:30:11 -0300

    Progressao Aritmética

--- PythonExercicios/ex051.py
+++ PythonExercicios/ex051.py
@@ -1,3 +1,8 @@
 #DESENVOLVA UM PROGRAMA QUE LEIA O 'PRIMEIRO TERMO' E A 'RAZÃO' DE UMA PA. NO FINAL, MOSTRE OS '10' PRIMEIROS TERMOS
 #DESSA PROGRESSÃO.
-
+t = int(input('Primeiro termo: '))
+r = int(input('Digite a razão: '))
+dt = t + (10 - 1) * r
+for c in range(t, dt + r, r):
+    print('{}'.format(c), end=" ")
+print('FIM!')
